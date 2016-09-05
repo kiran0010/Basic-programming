11836d9a926497c3b41f120c45cc83cd2b80fe0f
kiran0010 <totan0010@gmail.com>
2016-09-05 22:18:48 +0530

    my message

---
+++ myfile.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+int main()
+{
+    int n,i,j;
+    cin >> n;
+    std::vector<int> arr(n);
+    for(i = 0;i < n;i++)
+        {
+       cin >> arr[i];
+
+    }
+    for(j=i-1;j>=0;j--)
+    {
+        cout<<arr[j];
+        cout<<' ';
+    }
+    return 0;
+    }
